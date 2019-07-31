cd8b8798871178937a1a100e475b0eac37a91084
Daniel Leal <danielleal94@gmail.com>
2019-07-31 13:42:21 -0300

    init repo

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import { Container, NewPlaylist, Nav } from "./styles";
+
+import AddPlaylistIcon from "../../assets/images/add_playlist.svg";
+
+const Sidebar = () => (
+  <Container>
+    <div>
+      <Nav main>
+        <li>
+          <a href="">Navegar</a>
+        </li>
+        <li>
+          <a href="">Radio</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>SUA BIBLIOTECA</span>
+        </li>
+        <li>
+          <a href="">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="">Tocados recentemente</a>
+        </li>
+        <li>
+          <a href="">Músicas</a>
+        </li>
+        <li>
+          <a href="">Albums</a>
+        </li>
+        <li>
+          <a href="">Artistas</a>
+        </li>
+        <li>
+          <a href="">Estações</a>
+        </li>
+        <li>
+          <a href="">Arquivos Locais</a>
+        </li>
+        <li>
+          <a href="">Vídeos</a>
+        </li>
+        <li>
+          <a href="">Podcasts</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>PLAYLISTS</span>
+        </li>
+        <li>
+          <a href="">Melhores do Rock</a>
+        </li>
+      </Nav>
+    </div>
+    <NewPlaylist>
+      <img src={AddPlaylistIcon} alt="adicionar playlist" />
+      Nova Playlist
+    </NewPlaylist>
+  </Container>
+);
+
+export default Sidebar;
